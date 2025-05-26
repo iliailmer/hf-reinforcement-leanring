fd346d56bfd036d5734bccd9c8a53171721fb01d
Ilia Ilmer <contact@iliailmer.com>
2025-05-26 12:58:06 -0500

    adding env

---
+++ scripts/unit1/unit1.py
@@ -0,0 +1,11 @@
+import gymnasium
+
+# from huggingface_sb3 import load_from_hub, package_to_hub
+from huggingface_hub import (
+    notebook_login,
+)  # To log to our Hugging Face account to be able to upload models to the Hub.
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
