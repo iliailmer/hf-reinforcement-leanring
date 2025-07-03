df2dc015620d2d663b102f9b6624d74ac7bcd807
Ilia Ilmer <contact@iliailmer.com>
2025-07-02 20:51:47 -0400

    spaceinvaders long run on m1 mac

--- scripts/unit3/run.sh
+++ scripts/unit3/run.sh
@@ -1,1 +1,2 @@
-uv run python -m rl_zoo3.train --algo dqn --env SpaceInvadersNoFrameskip-v4 -f logs/ -c dqn.yml
+# uv run python -m rl_zoo3.train --algo dqn --env SpaceInvadersNoFrameskip-v4 -f logs/ -c dqn.yml
+uv run python -m rl_zoo3.push_to_hub --algo dqn --env SpaceInvadersNoFrameskip-v4 --repo-name dqn-SpaceInvadersNoFrameskip-v4 -orga flyingeli4 -f logs/
